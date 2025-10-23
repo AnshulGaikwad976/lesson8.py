d49c7319d5e10b0188d796ec760221be84900ca3
AnshulGaikwad976 <gaikwad.anshul14@gmail.com>
2025-10-23 18:39:51 +0200

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,11 @@
+students = {"Aisha": 85 , "Rahim":90 , "Nila":70}
+
+students["harry"] = 78
+
+students["Nila"] = 85
+
+del students["Rahim"]
+
+print("Aisha marks:" , students["Aisha"])
+
+print("all students and marks:" , students)
